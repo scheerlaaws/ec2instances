832184acf5ccbc8f3db746d2e433a2723e33622f
Srinivas Reddy Cheerla <srinivascheerla009@gmail.com>
2025-03-17 22:52:03 +0000

    Create main.tf
    file

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "instances" {
+  description = "List of instances with their configurations"
+  type = list(object({
+    instance_type = string
+    volume_type   = string
+    volume_size   = number
+    key_name      = string
+    iops          = number
+  }))
+}
+
+resource "aws_instance" "ec2_instances" {
+  count         = length(var.instances)
+  ami           = "ami-0024699f66c5b8fd7" # Replace with a valid AMI ID for your region
+  instance_type = var.instances[count.index].instance_type
+  key_name      = var.instances[count.index].key_name
+
+  root_block_device {
+    volume_type = var.instances[count.index].volume_type
+    volume_size = var.instances[count.index].volume_size
+    iops        = var.instances[count.index].iops
+  }
+
+  tags = {
+    Name = "Instance-${count.index + 1}"
+  }
+}
