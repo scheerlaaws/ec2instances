de998e98d15e7d30a63213e28fba146e2f04eafa
Srinivas Reddy Cheerla <srinivascheerla009@gmail.com>
2025-03-17 22:45:07 +0000

    Delete variable.tf
    delete

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "instances" {
-  description = "List of instances with their configurations"
-  type = list(object({
-    instance_type = string
-    volume_type   = string
-    volume_size   = number
-    key_name      = string
-  }))
-}
